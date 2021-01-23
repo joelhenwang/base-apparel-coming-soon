1269c9830010ce4e4be8473346b0433f4a0f0243
Joel <joelhenwang@gmail.com>
2021-01-23 00:27:42 +0000

    3rd commit - fix hero image on mobile

--- javascript/js_script.js
+++ javascript/js_script.js
@@ -1,6 +1,6 @@
 
 if(document.documentElement.clientWidth <= 768){
-    $(".hero-desktop").attr("src", "/images/hero-mobile.jpg");
+    $(".hero-desktop").attr("src", "");
 }
 else{
     $(".hero-desktop").attr("src", "/images/hero-desktop.jpg");
