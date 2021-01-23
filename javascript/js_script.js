036550aac25d8cc46438bb67b4483c55d820e401
Joel <joelhenwang@gmail.com>
2021-01-23 00:19:37 +0000

    2nd commit - final solution

---
+++ javascript/js_script.js
@@ -0,0 +1,11 @@
+
+if(document.documentElement.clientWidth <= 768){
+    $(".hero-desktop").attr("src", "/images/hero-mobile.jpg");
+}
+else{
+    $(".hero-desktop").attr("src", "/images/hero-desktop.jpg");
+}
+
+
+
+
